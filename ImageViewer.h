a709b739b5c21f77753d2db6c48f56ba3c3241dd
YutaroKikuchi <s1000614@sendai-nct.jp>
2015-10-16 10:57:14 +0900

    Update class ImageViewer  Update constructor  Update mathod showImgsbyLine  Update method matchpoint

--- ImageViewer.h
+++ ImageViewer.h
@@ -11,30 +11,32 @@
 class ImageViewer{
 private:
 	int numCam;
+	int lengh;
 	int combineImgbyLine(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end,cv::Mat out);
 	int combineImgbyRect(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end,cv::Mat out);
 	int DrawLine(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end,cv::Mat out);
-	int DrawLinesinLine(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end,cv::Mat out);
+	int DrawLinesinLine(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end,std::vector<ImageKeeper>::iterator prebegin,std::vector<ImageKeeper>::iterator preend,cv::Mat out);
 	int matchPoint(ImageKeeper from,ImageKeeper to,std::vector<cv::Point2f> &frompoints,std::vector<cv::Point2f> &topoints);
 
 public:
-	ImageViewer(int numcam){
+	ImageViewer(int numcam,int len){
 		numCam = numcam;
+		lengh = len;
 	}
 
 	int setWindow(){
 		cv::namedWindow("hoge",CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO);
 	}
 
-	int showImgsbyLine(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end){
+	int showImgsbyLine(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end,std::vector<ImageKeeper>::iterator prebegin,std::vector<ImageKeeper>::iterator preend){
 
 		cv::Mat output(cv::Size(begin[0].getIMG().cols*numCam,begin[0].getIMG().rows),CV_8UC3);
 
 		combineImgbyLine(begin,end,output);
 
-		DrawLinesinLine(begin,end,output);
+		DrawLinesinLine(begin,end,prebegin,preend,output);
 
-		cv::imshow("hoge",output);
+		cv::imshow("hoge",output); 
 
 		return 0;
 	}
@@ -94,25 +96,33 @@ int ImageViewer::combineImgbyRect(std::vector<ImageKeeper>::iterator begin,std::
 	}
 }
 
-int ImageViewer::DrawLinesinLine(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end,cv::Mat out){
+int ImageViewer::DrawLinesinLine(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end,std::vector<ImageKeeper>::iterator prebegin,std::vector<ImageKeeper>::iterator preend,cv::Mat out){
 
 	float col = (float)begin[0].getCols(); float row = (float)begin[0].getRows();
 
 	for(int i=0;i<end-begin;i++){
 
-		for(int j=i+1;j<end-begin;j++){
-			
-			std::vector<cv::Point2f> frompoints,topoints;
-			matchPoint(begin[i],begin[j],frompoints,topoints);
+		for(int j=0;j<end-begin;j++){
+		  if(i==j){
+		    continue;
+		  }
+		  std::vector<cv::Point2f> frompoints,topoints;
+		  matchPoint(prebegin[i],begin[j],frompoints,topoints);
+		  
+		  if(frompoints.size()>0 && topoints.size()>0){
+		    std::cout << "Match" << std::endl;
+		  }else{
+		    std::cout << "no Match" << std::endl;
+		  }
 
-			std::cout << "begin[" << i << "]  " << "begin[" << j << "]" << std::endl;
+		  std::cout << "begin[" << i << "]  " << "begin[" << j << "]" << std::endl;
 
-			for(int k=0;k<frompoints.size();k++){
-				cv::Point2f from = frompoints[k]+cv::Point2f(col*(i-1),0.0);
-				cv::Point2f to = topoints[k]+cv::Point2f(col*(j-1),0.0);
-
-				cv::line(out,from,to,cv::Scalar(0.0,0.0,200.0),2,8);
-			}
+		  for(int k=0;k<frompoints.size();k++){
+		    cv::Point2f from = frompoints[k]+cv::Point2f(col*i,0.0);
+		    cv::Point2f to = topoints[k]+cv::Point2f(col*j,0.0);
+		    
+		    cv::line(out,from,to,cv::Scalar(0.0,0.0,200.0),2,8);
+		  }
 		}
 
 	}
@@ -121,9 +131,14 @@ int ImageViewer::DrawLinesinLine(std::vector<ImageKeeper>::iterator begin,std::v
 
 int ImageViewer::matchPoint(ImageKeeper from,ImageKeeper to,std::vector<cv::Point2f> &frompoints,std::vector<cv::Point2f> &topoints){
 
+  std::cout << " start MatchPoint" << std::endl;
+
 	for(int i=0;i<from.getPointsSize();i++){
 		int fromFID = from.getFeatureID(i);
 		cv::Point2f point = to.getPointID(fromFID);
+
+		std::cout << "  getPointID(" << fromFID << ");" << std::endl;
+		std::cout << "  point = " << point << std::endl;
 
 		if(point != cv::Point2f(0.0,0.0)){
 			frompoints.push_back(from.getPointID(fromFID));
