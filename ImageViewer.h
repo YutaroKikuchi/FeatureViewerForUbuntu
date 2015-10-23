14e357366357715ddf080f2d3af67bf636c84327
YutaroKikuchi <s1000614@sendai-nct.jp>
2015-10-23 18:07:58 +0900

    Update class ImageViewer  Add method showImgsTheta  Add method combineImgTheta Thetaでの画像が表示できるようにした

--- ImageViewer.h
+++ ImageViewer.h
@@ -17,6 +17,7 @@ private:
 	int DrawLine(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end,cv::Mat out);
 	int DrawLinesinLine(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end,std::vector<ImageKeeper>::iterator prebegin,std::vector<ImageKeeper>::iterator preend,cv::Mat out);
 	int matchPoint(ImageKeeper from,ImageKeeper to,std::vector<cv::Point2f> &frompoints,std::vector<cv::Point2f> &topoints);
+	int combineImgTheta(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end,cv::Mat out);
 
 public:
 	ImageViewer(int numcam,int len){
@@ -57,6 +58,16 @@ public:
 		return 0;
 	}
 
+	int showImgsTheta(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end){
+
+		cv::Mat output(cv::Size(begin[0].getIMG().cols*6,begin[0].getIMG().rows*3),CV_8UC3);
+
+		combineImgTheta(begin,end,output);
+
+		cv::imshow("hoge",output);
+		return 1;
+	}
+
 };
 
 int ImageViewer::combineImgbyLine(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end,cv::Mat out){
@@ -94,7 +105,90 @@ int ImageViewer::combineImgbyRect(std::vector<ImageKeeper>::iterator begin,std::
 		rect.y = (int)(i/3)*rect.height;
 
 		cv::Mat roi(out,rect);
-		begin[i].getIMG().copyTo(roi);		
+		begin[i].getIMG().copyTo(roi);
+	}
+}
+
+int ImageViewer::combineImgTheta(std::vector<ImageKeeper>::iterator begin,std::vector<ImageKeeper>::iterator end,cv::Mat out){
+
+	if(end - begin !=numCam)
+		return -1;
+
+	cv::Rect rect;
+	rect.width = begin[0].getIMG().cols;
+	rect.height = begin[0].getIMG().rows;
+
+	cv::Mat black = cv::Mat::zeros(rect.width,rect.height,CV_8UC3);
+
+	for(int i=0;i<3;i++){
+
+		for(int j=0;j<6;j++){
+			rect.x = j*rect.width;
+			rect.y = i*rect.height;
+
+			cv::Mat roi(out,rect);
+
+			switch(i){
+				case 0:
+					switch(j){
+						case 0:
+							begin[5].getIMG().copyTo(roi);
+							break;
+						case 1:
+							begin[4].getIMG().copyTo(roi);
+							break;
+						case 3:
+							begin[10].getIMG().copyTo(roi);
+							break;
+						case 4:
+							begin[11].getIMG().copyTo(roi);
+							break;
+						default:
+							roi = black;
+					}
+					break;
+				case 1:
+					switch(j){
+						case 0:
+							begin[3].getIMG().copyTo(roi);
+							break;
+						case 1:
+							begin[0].getIMG().copyTo(roi);
+							break;
+						case 2:
+							begin[1].getIMG().copyTo(roi);
+							break;
+						case 3:
+							begin[9].getIMG().copyTo(roi);
+							break;
+						case 4:
+							begin[6].getIMG().copyTo(roi);
+							break;
+						case 5:
+							begin[7].getIMG().copyTo(roi);
+							break;
+						default:
+							roi = black;
+					}
+					break;
+				case 2:
+					switch(j){
+						case 1:
+							begin[2].getIMG().copyTo(roi);
+							break;
+						case 4:
+							begin[8].getIMG().copyTo(roi);
+							break;
+						default:
+							roi = black;
+					}
+					break;
+				default:
+					break;
+			}
+
+		}
+
 	}
 }
 
